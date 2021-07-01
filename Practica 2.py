b29427b02d6179e3223a90e96d34dfa9aab156e0
Luis Manuel Méndez <electronicaparaelmundo@gmail.com>
2021-06-30 21:29:05 -0700

    Versión 2

---
+++ Practica 2.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 23 12:58:47 2021
+
+@author: HP
+"""
+"""
+edad = 47
+altura = 1
+altura = 8.97
+perro = 4
+print(edad,     altura,     perro)
+print(type(edad),type(altura),type(perro))
+"""
+"""
+a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(-a+b)
+print(a**b)
+print((a*b)+(b**a))
+"""
+"""
+import math
+print(math.sqrt(16))
+
+print(pow(25,0.5))
+"""
+a = 1
+b = 3
+c = 2
+"""
+print(pow(b**2-(4*a*c),0.5)/(2*a))
+"""
+print((-b+pow(b**2-(4*a*c),0.5))/(2*a))
